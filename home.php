e2736e93a65f98c7b1796e2a423f06426b989bac
Xùdōng Wú <wooxudong@gmail.com>
2015-05-11 22:34:01 +0800

    markup for the whole website is done

---
+++ home.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Xudong Online Store</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/shop-homepage.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="home.php">Xudong Online Store</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+<!--             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div> -->
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            
+
+            <div class="col-md-12">
+
+                <div class="row carousel-holder">
+
+                    <div class="col-md-12">
+                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="item active">
+                                    <img class="slide-image" src="img/courasell1.png" alt="">
+                                </div>
+                                <div class="item">
+                                    <img class="slide-image" src="img/courasell2.png" alt="">
+                                </div>
+                                <div class="item">
+                                    <img class="slide-image" src="img/courasell3.png" alt="">
+                                </div>
+                            </div>
+                            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left"></span>
+                            </a>
+                            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right"></span>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="row">
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="img/decolamp.png" alt="">
+                            <div class="caption">
+                                <h4 class="pull-right">$34.99</h4>
+                                <h4>Deco Lamp</h4>
+                                <p>Looking for a desk lamp that is both functional and goodlooking? This Deco Lamp will do the trick thanks to the characteristic of PMMA. <br> By <a target="_blank" href="http://iriswang329.wix.com/wangyaweidesign#!contour/cd6d">Yawei Wang Design</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">190 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="img/readtable.png" alt="">
+                            <div class="caption">
+                                <h4 class="pull-right">$164.99</h4>
+                                <h4>Read Table</h4>
+                                <p>The Read Table is here to make sure that you could both read and store the reading materials in case that you might want to pick them up every now and then.<br> By <a target="_blank" href="http://iriswang329.wix.com/wangyaweidesign#!contour/cd6d">Yawei Wang Design</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">235 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="img/teatable.png" alt="">
+                            <div class="caption">
+                                <h4 class="pull-right">$274.99</h4>
+                                <h4>Tea Table</h4>
+                                <p>Simplifing the shape of the ancient Chinese tea table and adding the combination of thin black metal lines and glass panel to add a delicate touch.<br> By <a target="_blank" href="http://iriswang329.wix.com/wangyaweidesign#!contour/cd6d">Yawei Wang Design</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">131 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="img/armchair.png" alt="">
+                            <div class="caption">
+                                <h4 class="pull-right">$184.99</h4>
+                                <h4>Arm Chair</h4>
+                                <p>The highly contrast between the thin black metal tube lines and solid body makes this chair look delicate and chic.<br> By <a target="_blank" href="http://iriswang329.wix.com/wangyaweidesign#!contour/cd6d">Yawei Wang Design</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">128 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="img/highchair.png" alt="">
+                            <div class="caption">
+                                <h4 class="pull-right">$94.99</h4>
+                                <h4>High Chair</h4>
+                                <p>How can a furniture series complete without a high chair? If you have a bar area in your home, you should definately check this out.<br> By <a target="_blank" href="http://iriswang329.wix.com/wangyaweidesign#!contour/cd6d">Yawei Wang Design</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">88 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="img/loungechair.png" alt="">
+                            <div class="caption">
+                                <h4 class="pull-right">$294.99</h4>
+                                <h4>Lounge Chair</h4>
+                                <p>This Lounge Chair here with fluent edge lines and sea inspired gradient soft color cushions will make your room instantly appear relaxing and comfy.<br> By <a target="_blank" href="http://iriswang329.wix.com/wangyaweidesign#!contour/cd6d">Yawei Wang Design</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">318 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-sm-4 col-lg-4 col-md-4">
+                 <h4><a href="#">Check out with Paypal</a></h4>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <div class="container">
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Xudong Online Store 2015</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
